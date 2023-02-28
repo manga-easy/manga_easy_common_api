e0e47d661ef5455eb0b40376f591fb5c825b994b
lucas <lucas.cardo123@hotmail.com>
2023-02-28 13:25:57 -0400

    Add generics in DatabaseService

--- lib/src/database_service.dart
+++ lib/src/database_service.dart
@@ -1,10 +1,10 @@
 import 'package:common_api/src/custom_filter_log.dart';
 import 'package:logger/logger.dart';
 
-abstract class DataBaseService {
+abstract class DataBaseService<T> {
   final logger = Logger(filter: CustomFilterLog());
 
-  Future exec(String query);
+  Future<T> exec(String query);
 
   void log(String query, e) {
     logger.d(
